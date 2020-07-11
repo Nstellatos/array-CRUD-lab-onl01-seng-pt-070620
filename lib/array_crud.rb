38362c456494266148fe6b1ea707fdd99a8880cc
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-11 00:23:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array ["dog","cat","bunny","snake"]
+  array.push "horse"
 end
 
 def add_element_to_start_of_array(array, element)

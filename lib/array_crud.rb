292927a8f8891540b755465d7d9dc9a4831d8b21
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-11 00:41:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,12 +20,14 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["arrays!"]
-  array.shift
+  array.pop
+  
   
 end
 
 def remove_element_from_start_of_array(array)
   array = ["wow"]
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
